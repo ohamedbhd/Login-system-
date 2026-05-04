4ee14d8941d54e8a3e1e9a7aa43bd8af61389104
ohamedbhd <161217208+ohamedbhd@users.noreply.github.com>
2026-05-04 22:13:18 +0300

    Add files via upload
    This is an advanced C++ console application that starts with a login system and offers multiple features through a master menu. Features: 
    - User Authentication (User Name and Password) 
    - Calculator (support +, -, *,%, ^) - Check if the number is positive or negative 
    - Check if the number is even or individual 
    - Student management system (add, show, Search)
     - In this project, arrays, loops, functions, and problem-solving C++ have been used.

---
+++ Login System.cpp
@@ -0,0 +1,305 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+// لاخذ سطر جديد
+void Design()
+{
+  cout <<"\n";
+}
+
+      // ######## Login => نظام تسجيل الدخول ########
+
+// الكود ده بيطلب من المستخدم يدخل اليوزر والباسورده
+bool login ()
+{
+  string UserName, Passw;
+  string my_Name= "mohamed", my_passw= "1234";
+  cout <<"   ■■■■■■■■■■■■■■■■■■■■ login ■■■■■■■■■■■■■■■■■■■■\n \n";
+  cout << "enter your name: ";
+  cin >> UserName;
+  
+  cout << "enter your password: ";
+  cin >> Passw;
+  Design();
+  return (UserName==my_Name&&Passw==my_passw);
+}
+
+         // ######## Calculator => نظام الة حاسبة ########
+
+// الكود ده الة حاسبة بيطلب من المستخدم يدخل الرقم لاول وبعدين العملية الحسابية وبعدين يدخل الرقم التانى وينفذ العملية الحسابية كلها
+
+void Calculator ()
+{
+  double num1, num2;
+  char op;
+  
+  cout <<"   ■■■■■■■■■■■■■■■ slot calculator ■■■■■■■■■■■■■■■\n\n";
+  
+  cout << "enter Number ( 1 ): ";
+  cin >> num1;
+  
+  cout << "enter your [ + , - , * , / , % , ^ ]: ";
+  cin >> op;
+  
+  cout << "enter Number ( 2 ): ";
+  cin >> num2;
+  Design();
+  
+  switch (op)
+  {
+    case '+' :
+    cout << num1 << " + " << num2<<" = "<< num1 + num2 << endl;
+    break;
+    case '-' :
+    cout << num1 << " - " << num2 << " = " << num1 - num2 << endl ;
+    break;
+    case '*' :
+    cout << num1 << " * " << num2 << " = " << num1 * num2 << endl ;
+    break;
+    case '/' :
+    if (num2 ==0)
+    {
+      cout << "The number you entered is not divisible by zero\n";
+    }
+    else
+    {
+      cout << num1 << " / " << num2 << " = " << num1 / num2 << endl ; 
+    }
+    break;
+    case '%' :
+    cout << num1 << " % " << num2 << " = " << int(num1) % int(num2) << endl ;
+    break;
+    case '^' :
+    cout << num1 << " ^ " << num2 << " = " << pow(num1 , num2) << endl ;
+    break;
+    default:
+    cout <<"Eerro";
+  }
+}
+ 
+              // ######## positive or negative => نظام الموجب والسالب ########
+ 
+//الكود ده بيطلب من المستخدم يدخل رقم ويشوف الرقم ده موجب ولا سالب
+void checkSign ()
+{
+  int num;
+  cout <<"enter your number: ";
+  cin >> num;
+  Design();
+  
+  if (num >=0)
+  {
+    cout <<"The number is positive \n";
+    Design();
+  }
+  else
+  {
+    cout <<"The number is negative \n";
+    Design();
+  }
+}
+
+             // ######## Even or Odd => نظام الفردى والزوجى  ########
+
+// الكود ده بيطلب من المستخدم يدخل رقم ويشوف الرقم ده زوجى ولا فردى
+void checkEvenOdd()
+{
+  int num;
+  cout <<"enter your number: ";
+  cin >> num;
+  Design();
+  if(num % 2 == 0)
+  {
+    cout <<"Even number\n";
+    Design();
+  }
+  else
+  {
+    cout <<"Odd number\n";
+    Design();
+  }
+}
+
+          // ######## Student management system => نظام اداه الطلاب ########
+
+// عندك اربع متغيرات لاسم والعمر ولايدى ولمتغير الرابع ده بيحفظ بيانات كل طالب لوحده بحيث ان البيانات كل طالب متتلغبش فى بعضstring
+string name[100];
+int age[100];
+int ids[100];
+int countStudent = 0;
+
+//الكود ده بيطلب من المستخدم يدخل لاسم والعمر ولايدى بتاعة 
+
+void addStudent()
+{
+  cout <<"enter your name: ";
+  cin >>name[countStudent];
+  cout <<"enter your age: ";
+  cin >>age[countStudent];
+  
+  cout <<"enter your ID: ";
+  cin >>ids[countStudent];
+  
+  countStudent++;
+}
+
+// الكود ده لما المستخدم يكون عايز يعرض الطلاب ال فى النظام
+
+void showStudent ()
+{
+  for (int i=0; i < countStudent; i++)
+  {
+    cout << "nam: " << name[i];
+    cout << "  age: " << age[i];
+    cout << "  ID: " << ids[i];
+  }
+}
+
+// الكود ده لما المستخدم يكون عايز يبحث عن طالب موجود فى النظام بى لايدى بتاعة
+
+void searchStudent()
+{
+  int id;
+  
+  cout <<"enter my student ID: ";
+  cin >>id;
+  for (int i=0; i < countStudent; i++)
+  {
+    if (ids[i] == id)
+    {
+      cout << "Found: " << name[i];
+      return;
+    }
+    else
+    {
+      cout <<"No found";
+    }
+  }
+}
+
+// الكود ده هيظهر لى المستخدم اربع اختيارات كل اختيار منهم لية كود والكود موجود فى الدول ال احنا عملنها فوق وعملنا استدعاء ليهم عشان الكود يشتغل مضبوط
+
+void StudentMenu()
+{
+  int che;
+  clearScreen();
+  do 
+  {
+    clearScreen();
+    Design();
+    cout <<"1- add\n";
+    cout <<"2- Show\n";
+    cout <<"3- Search\n";
+    cout <<"4- Back\n";
+    cout <<"enter your choice: " ;    
+    cin >>che;
+    switch (che)
+    {
+      case 1:
+      addStudent();
+      break;
+      case 2:    
+      showStudent();    
+      break;   
+      case 3: 
+      searchStudent(); 
+      break;    
+    }
+  } while (che !=4);
+}
+
+                  // ######## مكان تنفيذ كل لاكواد ########
+
+//الكود ده فية القائمة الرائسية ال رح تظهر لى مستخدم بعد مايدخل اليوزر والباسورد رح يظهر لية تسع اختيارات والمستخدم يدخل رقم العملية ال محتاجها وفى هذا القائمة الرئسية مجمع كل الدول فيها بحيث العملية تكون سهلة على المستخدم
+
+void Show_Menu()
+{
+  int choice;
+  while (true)
+  {
+    clearScreen();
+    Design();
+    cout << "   ■■■■■■■■■■■■■■■■■■ Main Menu ■■■■■■■■■■■■■■■■■■ \n \n";
+    cout << "1- Python\n";
+    cout << "2- C++\n";
+    cout << "3- java\n";
+    cout << "4- java Script\n";
+    cout << "5- Calculator\n"; 
+    cout << "6- Positive or negative\n";
+    cout << "7- Even or Odd\n";
+    cout << "8- student system\n";
+    cout << "9- Exit\n\n";
+    Design();
+    cout << "                  ■■■■■■■■■■■■■■■■■■ \n";    
+    cout << "enter your choice: ";    
+    cin >> choice;    
+    Design();
+    switch(choice)
+    {
+      case 1:
+      cout<<"Python is used in Cyber Security and AI\n";
+      break;
+      case 2:
+      cout<<"C++ is used in systems and performance applications\n";
+      break;
+      case 3:
+      cout<<"Java is used in large systems and Android apps\n";
+      break;
+      case 4:
+      cout<<"JavaScript is used for web development\n";    
+      break;
+      case 5:
+      Calculator();
+      break;
+      case 6 :
+      checkSign();
+      break;
+      case 7 :
+      checkEvenOdd();
+      break;
+      case 8 :
+      StudentMenu();
+      break;
+      case 9 :
+      cout<<"Goodbye\n";
+      return;
+      default:
+      cout <<"Invalid choice\n";
+    }
+  }
+}
+
+                  // ######## ده الدالة لأساسية لتنفيذ لاكواد ########
+
+// فى هذا الدلة هيا الدلة الرائسية ال رح تشغل الكود بلكامل وموجود فيعا عملية المحاولة لما المتسخدم يدخل اليوزر والباسورد المستخدم لية ثلات محاولات فقط ان عمل اكثر من ثلاث محاولات رح يخرجو من النظام بلكامل 
+
+int main()
+{
+  int attempts = 3;
+  bool TheSuccess = false;
+
+  while (attempts > 0 )
+  {
+    if (login() == 1)
+    {
+      cout << "Signed in successfully\n";    
+      TheSuccess= true;
+      break;
+    }
+    else
+    {
+      attempts--;
+      cout <<"Wrong data please try again: " << attempts <<endl;
+      Design();
+    }
+  }
+  if (TheSuccess)
+  {
+    Show_Menu();
+  }
+  else
+  {
+    cout <<"\nAccount locked \n\n";
+  }
+  return 0;
+}
